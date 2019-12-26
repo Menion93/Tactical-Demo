3c3659a9a8b13874ba38355dfe98eaaf1c05e335
menion93 <andrea.salvoni93@gmail.com>
2019-12-26 21:05:16 +0100

    add base spawner class

---
+++ Source/TacticalGame/Spawner.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Spawner.h"
+#include "Engine/World.h"
+#include "UObject/ConstructorHelpers.h"
+#include "TacticalGameGameMode.h"
+#include "ControllableCharacter.h"
+
+// Sets default values
+ASpawner::ASpawner()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	static ConstructorHelpers::FClassFinder<AControllableCharacter> Player1(TEXT("/Game/TopDownCPP/Blueprints/Characters/Player1"));
+	static ConstructorHelpers::FClassFinder<AControllableCharacter> Player2(TEXT("/Game/TopDownCPP/Blueprints/Characters/Player2"));
+
+	if (Player1.Class != NULL)
+	{
+		party.Add(Player1.Class);
+	}
+
+	if (Player2.Class != NULL)
+	{
+		party.Add(Player2.Class);
+	}
+}
+
+// Called when the game starts or when spawned
+void ASpawner::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ASpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASpawner::SpawnCharacters()
+{
+	for (int pos = 0; pos < party.Num(); pos++)
+	{
+		UWorld* World = GetWorld();
+
+		if (World && Spawns.Num() > pos)
+		{
+			AControllableCharacter* character = World->SpawnActor<AControllableCharacter>(party[pos], Spawns[pos]->GetActorLocation(), FRotator::ZeroRotator);
+			ATacticalGameGameMode* GM = Cast<ATacticalGameGameMode>(GetWorld()->GetAuthGameMode());
+			GM->GameDirector->TileMap->SnapToGrid(character);
+		}
+	}
+}
---
+++ Source/TacticalGame/Spawner.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ATileMapSet.h"
+#include "Spawner.generated.h"
+
+UCLASS()
+class TACTICALGAME_API ASpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawner();
+
+	TArray<TSubclassOf<class AControllableCharacter>> party;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<AActor*> Spawns;
+
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	void SpawnCharacters();
+};
