bc6e122d9438c989b98a48dfb22f5e4bb7ae470e
menion93 <andrea.salvoni93@gmail.com>
2019-12-30 11:16:01 +0100

    add property in editor for the map tiles

--- Source/TacticalGame/FTile.h
+++ Source/TacticalGame/FTile.h
@@ -7,8 +7,18 @@ USTRUCT()
 struct FTile
 {
 	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
 		bool IsObstacle = false;
+		
+	UPROPERTY(EditAnywhere)
 		FVector TileCenter = FVector(0, 0, 0);
+	
+	UPROPERTY(EditAnywhere)
 		FVector SurfaceNormal = FVector(0, 0, 0);
+	
+		TArray<FTile*> Neighbours;
+	
+	UPROPERTY(EditAnywhere)
 		bool IsCover = false;
 };
