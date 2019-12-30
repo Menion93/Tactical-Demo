df6abfa9e80f89ab82ade2a404661595e3bce5d2
menion93 <andrea.salvoni93@gmail.com>
2019-12-30 11:14:42 +0100

    add character state model

---
+++ Source/TacticalGame/CharacterState.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ControllableCharacter.h"
+#include "Bag.h"
+#include "CharacterState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TACTICALGAME_API UCharacterState : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UCharacterState();
+
+	float health;
+	
+	UBag* Bag;
+	//UArmorDescriptor* armors;
+
+public:
+	void SetParentActor(AControllableCharacter* Character);
+	
+};
