7157d3faac3333f1c7c2f5fb5e18f1e77817d860
menion93 <andrea.salvoni93@gmail.com>
2020-01-02 15:16:39 +0100

    add custom player controller, remove default pawn class

--- Source/TacticalGame/TacticalGameGameMode.cpp
+++ Source/TacticalGame/TacticalGameGameMode.cpp
@@ -1,19 +1,22 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "TacticalGameGameMode.h"
-#include "TacticalGamePlayerController.h"
 #include "BattleGameState.h"
 #include "Engine/World.h"
 #include "ControllableCharacter.h"
 #include "Spawner.h"
 #include "Kismet/GameplayStatics.h"
 #include "UObject/ConstructorHelpers.h"
+#include "GPlayerController.h"
 
 
 ATacticalGameGameMode::ATacticalGameGameMode()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	GameStateClass = ABattleGameState::StaticClass();
+	// Failsafe
+	PlayerControllerClass = AGPlayerController::StaticClass();
+	DefaultPawnClass = nullptr;
 
 	GameDirector = NewObject<UGameDirector>(this, TEXT("GameDirector"));
 }
@@ -23,6 +26,7 @@ void ATacticalGameGameMode::StartPlay()
 {
 	Super::StartPlay();
 	GameDirector->Init();
+	CurrentMode = GameModeE::GSE_Battle;
 }
 
 void ATacticalGameGameMode::Tick(float DeltaSeconds)
@@ -33,14 +37,7 @@ void ATacticalGameGameMode::Tick(float DeltaSeconds)
 	if (CurrentMode == GameModeE::GSE_Battle)
 	{
 		ABattleGameState* BState = GetGameState<ABattleGameState>();
-
 		BState->PlayTurn();
 	}
 
-}
-
-
-void ATacticalGameGameMode::SwapGameState(AGameStateBase* GameStateVariable)
-{
-	GameStateClass = GameStateVariable->StaticClass();
 }
