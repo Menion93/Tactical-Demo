f85c266980a6565dc59b4652fde2bfb87cd69522
Menion <andrea.salvoni93@gmail.com>
2020-03-08 20:00:17 +0100

    add base ui blueprint class

---
+++ Source/TacticalGame/BattleUI.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BattleUI.h"
+
+//UMain_HUD::UMain_HUD(FObjectInitializer& ObjectInitializer)
+//	: UUserWidget(ObjectInitializer) // Call the UUserWidget's constructor.
+//{
+//	CanvasPanel = CreateDefaultSubobject<UCanvasPanel>(TEXT("Canvas Panel"));
+//}
---
+++ Source/TacticalGame/BattleUI.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BattleUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class TACTICALGAME_API UBattleUI : public UUserWidget
+{
+	GENERATED_BODY()
+
+	
+};
