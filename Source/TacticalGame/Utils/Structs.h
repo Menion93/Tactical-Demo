4659176564ef6d58ecb1790d37dcd5f363c9bf66
Menion <andrea.salvoni93@gmail.com>
2020-03-08 20:11:11 +0100

    add structs for array containers

--- Source/TacticalGame/Utils/Structs.h
+++ Source/TacticalGame/Utils/Structs.h
@@ -3,11 +3,32 @@
 #include "CoreMinimal.h"
 #include "Structs.generated.h"
 
-USTRUCT()
-struct FArrayOfArray
+class UCharacterState;
+
+USTRUCT(BlueprintType)
+struct FCharacterStateArray
 {
 	GENERATED_BODY()
 
-	UPROPERTY()
-	TArray <FVector> Array;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<UCharacterState*> Array;
+};
+
+USTRUCT(BlueprintType)
+struct FVectorArray
+{
+	GENERATED_BODY()
+
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<FVector> Array;
+};
+
+
+USTRUCT(BlueprintType)
+struct FCharacterClassArray
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<TSubclassOf<class AControllableCharacter>> Array;
 };
