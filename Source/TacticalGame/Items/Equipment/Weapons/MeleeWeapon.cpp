018f9782fcb7b6a554f4bbe888d222790add1463
Menion <andrea.salvoni93@gmail.com>
2020-05-21 21:49:47 +0200

    add melee weapon simulation

--- Source/TacticalGame/Items/Equipment/Weapons/MeleeWeapon.cpp
+++ Source/TacticalGame/Items/Equipment/Weapons/MeleeWeapon.cpp
@@ -4,6 +4,7 @@
 #include "MeleeWeapon.h"
 #include "Grid/Grid.h"
 #include "Characters/CharacterState.h"
+#include "Globals/TacticalGameMode.h"
 #include "Utils/BattleRangeUtils.h"
 #include "Utils/GridUtils.h"
 
@@ -19,20 +20,61 @@ bool UMeleeWeapon::IsInRange_Implementation(AGCharacter* MyCharacter, AGCharacte
 
 bool UMeleeWeapon::IsInRangeFromTile_Implementation(FTile Tile, AGCharacter* MyCharacter, AGCharacter* Target)
 {
-	return UBattleRangeUtils::IsInMeleeRangeFromTile(Tile, MyCharacter, Target);;
+	return UBattleRangeUtils::IsInMeleeRangeFromTile(Tile, MyCharacter, Target);
 }
 
-UAction* UMeleeWeapon::GetAction_Implementation()
+UAction* UMeleeWeapon::GetAction_Implementation(AGCharacter* Subject, AGCharacter* Target, FTileIndex FromTile)
 {
-	return nullptr;
+	UMeleeAttackAction* Action = NewObject<UMeleeAttackAction>(this, ActionClass);
+	ATacticalGameMode* GameMode = Cast<ATacticalGameMode>(GetWorld()->GetAuthGameMode());
+	Action->Init(GameMode->BattleManager, this, Subject, Target, FromTile);
+	return Action;
 }
 
 void UMeleeWeapon::SimulateAction_Implementation(AGCharacter* Character, AGCharacter* Target)
 {
+	float Distance = FVector::Distance(Character->GetActorLocation(), Target->GetActorLocation());
+	float MyAccuracy = GetAccuracy();
 
+	float MyDamage = GetDamage();
+
+	float MyCriticalChance = GetCriticalChance();
+	float MyCriticalDamage = GetCriticalDamage();
+
+	AttackSimulation = TArray<FMeleeHitSim>();
+
+	for (int i = 0; i < AttacksPerTurn; i++)
+	{
+		FMeleeHitSim Attack;
+
+		int HitProb = FMath::FRand();
+
+		Attack.HasHit = HitProb < MyAccuracy;
+
+		// Has It
+		if (Attack.HasHit)
+		{
+			int CritProb = FMath::FRand();
+
+			Attack.HasCritted = CritProb < MyCriticalChance;
+			Attack.Damage = MyDamage * (1 + MyCriticalDamage * int(Attack.HasCritted));
+		}
+
+		AttackSimulation.Add(Attack);
+	}
 }
 
 void UMeleeWeapon::ApplyAction_Implementation(AGCharacter* Character)
 {
 
+}
+
+float UMeleeWeapon::GetAccuracy()
+{
+	return BaseAccuracy + Accuracy + AccuracyRoll;
+}
+
+float UMeleeWeapon::GetDamage()
+{
+	return BaseDamage + Damage + DamageRoll;
 }
--- Source/TacticalGame/Items/Equipment/Weapons/MeleeWeapon.h
+++ Source/TacticalGame/Items/Equipment/Weapons/MeleeWeapon.h
@@ -4,11 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "Items/Equipment/Weapons/WeaponCore.h"
+#include "BattleSystem/Actions/MeleeAttackAction.h"
+#include "Utils/SimStructs.h"
 #include "MeleeWeapon.generated.h"
 
 /**
  * 
  */
+
 UCLASS()
 class TACTICALGAME_API UMeleeWeapon : public UWeaponCore
 {
@@ -16,6 +19,31 @@ class TACTICALGAME_API UMeleeWeapon : public UWeaponCore
 
 public:
 	UMeleeWeapon();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+		int AttacksPerTurn;
+
+		// BULLET DAMAGE PROPERTIES
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+		float BaseDamage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+		float Damage;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Damage")
+		float DamageRoll;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+		float MinDamage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+		float MaxDamage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+		float MinRollDamage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+		float MaxRollDamage;
 
 	// ACCURACY PROPERTIES
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Accuracy")
@@ -40,12 +68,20 @@ public:
 		float MaxRollAccuracy;
 
 public:
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FMeleeHitSim> AttackSimulation;
+
+
+public:
 	virtual bool IsInRange_Implementation(AGCharacter* MyCharacter, AGCharacter* Target) override;
 	virtual bool IsInRangeFromTile_Implementation(FTile Tile, AGCharacter* MyCharacter, AGCharacter* Target) override;
 
 	virtual void SimulateAction_Implementation(AGCharacter* Character, AGCharacter* Target) override;
 	virtual void ApplyAction_Implementation(AGCharacter* Target) override;
 
-	virtual UAction* GetAction_Implementation() override;
+	virtual UAction* GetAction_Implementation(AGCharacter* Subject, AGCharacter* Target, FTileIndex FromTile) override;
 
+public:
+	float GetAccuracy();
+	float GetDamage();
 };
